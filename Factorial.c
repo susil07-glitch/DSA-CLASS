5bbe3cec2c8a949f92383c32c9b0076474ac52aa
Sushil Sharma <214009577+susil07-glitch@users.noreply.github.com>
2026-06-03 09:34:15 +0545

    factorial using c

---
+++ Factorial.c
@@ -0,0 +1,30 @@
+
+
+#include<stdio.h>
+
+
+int main(){
+
+
+    int n,i ;
+    unsigned long long factorial =1 ;
+
+    printf("Enter a number to find its factorial:");
+    scanf("%d",&n);
+
+    if(n<0){
+        printf("Error ! Factorial of a negative number doesn't exist ");
+
+    }else{
+        for(i=1;i<=n;i++){
+            factorial *=i;
+
+        }
+        printf("Factorial of %d = %llu",n,factorial);
+
+    }
+
+    return 0;
+
+}
+
